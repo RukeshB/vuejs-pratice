77b772f5beb236b3c4ae344736b7e99b436fd168
RukeshB <rukeshbasukala@gmail.com>
2020-03-12 15:24:48 +0545

    loop in vuejs

---
+++ loop/loop.js
@@ -0,0 +1,11 @@
+var app1 = new Vue({
+	el   : '#app1',
+	data : {
+		users : [
+			{ fname: 'Ram', lname: 'Suwal', gender: 'male', age: 20 },
+			{ fname: 'Shyam', lname: 'Duwal', gender: 'male', age: 21 },
+			{ fname: 'Kamless', lname: 'Suwal', gender: 'male', age: 22 },
+			{ fname: 'Season', lname: 'Naga', gender: 'female', age: 22 }
+		]
+	}
+});
